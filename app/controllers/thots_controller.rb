4a3e9b8a2276f189e4982f05fb0122335ac896cf
Michael Aranda <michaelaranda0.web@gmail.com>
2017-01-03 00:36:45 -0800

    Adds new route (/thots/new) for creating new Thots (title, details, category). Also creates specified category if it does not already exist.

--- app/controllers/thots_controller.rb
+++ app/controllers/thots_controller.rb
@@ -7,4 +7,25 @@ class ThotsController < ApplicationController
     category_id = Category.find_by(url_friendly_title: params[:category_title]).id
     @thots = Thot.where(category_id: category_id)
   end
+
+  def new
+  end
+
+  def create
+    category_param = params[:category]
+    category = Category.find_by(title: category_param)
+
+    if category.nil?
+      category = Category.create!(
+          title: category_param,
+          url_friendly_title: category_param.gsub(' ', '_').underscore
+      )
+    end
+
+    @created_thot = Thot.create!(
+        title: params[:title],
+        details: params[:details],
+        category_id: category.id
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'thots/index'
-
   get 'thots/category'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -12,6 +11,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+  resources :thots, only: [:new, :create]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
